b983ad09228ae633eaa651d4ce2492eebc09372b
Damon Fedorick <95268795+Ddemon26@users.noreply.github.com>
2025-08-23 04:36:22 -0700

    Add MLBrain and MlBrainData assets and scripts
    Introduced MLBrain and MlBrainData ScriptableObjects for ML-Agents integration, including their Unity asset and meta files. Updated .gitignore to exclude ML-Agents folders. These changes provide a foundation for managing neural network models within the Unity project.

---
+++ Assets/_Damon/TCS.MLAgents/Runtime/Unity/MLBrain.cs
@@ -0,0 +1,20 @@
+﻿using Unity.Barracuda;
+using UnityEngine.Serialization;
+namespace TCS.MLAgents._Damon.TCS.MLAgents.Runtime.Unity {
+    [CreateAssetMenu( menuName = "Create MLBrain", fileName = "MLBrain", order = 0 )] 
+    public class MlBrain : NNModel {
+        [SerializeField] MlBrainData m_modelData;
+        
+        public string ModelPath => m_modelData != null ? m_modelData.name : "No Model Assigned";
+        
+        public override string ToString() {
+            return ModelPath;
+        }
+        
+        void OnValidate() {
+            if (m_modelData != null) {
+                modelData = m_modelData;
+            }
+        }
+    }
+}
---
+++ Assets/_Damon/TCS.MLAgents/Runtime/Unity/MlBrainData.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Barracuda;
+namespace TCS.MLAgents._Damon.TCS.MLAgents.Runtime.Unity {
+    [CreateAssetMenu( menuName = "Create MlBrainData", fileName = "MlBrainData", order = 0 )] 
+    public class MlBrainData : NNModelData {
+        public byte[] Data {
+            get => Value;
+            set => Value = value;
+        }
+    }
+}
