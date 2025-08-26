ba8f87b64c4f2fb92b57b32385ae8fdf3499344d
Damon Fedorick <95268795+Ddemon26@users.noreply.github.com>
2025-08-26 00:46:20 -0700

    Add ChannelFollowersResponse model and docs
    Introduces the ChannelFollowersResponse C# model for Twitch channel followers API responses, including related classes and JSON property mappings. Adds documentation and example JSON for the channel followers endpoint.

---
+++ Assets/Project/Scripts/TwitchRevamp/Runtime/Models/ChannelFollowersResponse.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace TwitchRevamp.Models {
+    [Serializable]
+    public class ChannelFollowersResponse {
+        [JsonProperty("data")]
+        public List<ChannelFollower> Data { get; set; }
+       /* "pagination": {
+    "cursor": "eyJiIjpudWxsLCJhIjp7Ik9mZnNldCI6Mn19"
+  },*/
+        [JsonProperty("pagination")]
+        public Pagination Pagination { get; set; }
+        
+        [JsonProperty("total")]
+        public int Total { get; set; }
+    }
+    
+    [Serializable]
+    public class ChannelFollower {
+        [JsonProperty("followed_at")]
+        public string FollowedAt { get; set; }
+        
+        [JsonProperty("user_id")]
+        public string UserId { get; set; }
+        
+        [JsonProperty("user_login")]
+        public string UserLogin { get; set; }
+        
+        [JsonProperty("user_name")]
+        public string UserName { get; set; }
+    }
+
+    [Serializable]
+    public class Pagination {
+        [JsonProperty("cursor")]
+        public string Cursor { get; set; }
+    }
+}
