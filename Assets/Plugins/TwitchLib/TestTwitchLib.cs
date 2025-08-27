66b107dbb02307e99984ebe7d9f9324286d07e9d
Damon Fedorick <95268795+Ddemon26@users.noreply.github.com>
2025-08-27 02:18:37 -0700

    Update TestTwitchLib.cs

--- Assets/Plugins/TwitchLib/TestTwitchLib.cs
+++ Assets/Plugins/TwitchLib/TestTwitchLib.cs
@@ -1,14 +1,23 @@
+using TCS.Utils;
 using TwitchLib.Client.Events;
 using TwitchSharp;
 using UnityEngine;
 
 public class TestTwitchLib : MonoBehaviour {
+    public string m_userName;
+    public string m_accessToken;
+
+    [TextArea]
+    public string m_testMessage = "Test Message";
+    
     TwitchUserBot bot;
 
     void Awake() {
         bot = TwitchBotFactory.CreateForUnity(
-            username: "your_twitch_username",
-            accessToken: "access_token_here", // Get this from https://twitchtokengenerator.com/
+            //username: "your_twitch_username",
+            m_userName,
+            //accessToken: "access_token_here", // Get this from https://twitchtokengenerator.com/ DONT SHOW TO ANYONE
+            m_accessToken,
             defaultChannel: "", // normally your username with a # prefix
             true // turn on logging
         );
@@ -26,7 +35,12 @@ public class TestTwitchLib : MonoBehaviour {
         if (sender == null) return;
         Debug.Log( e.Data );
     }
+    
     void SendChatMessage(string message = "Hello from Unity!") {
         bot.SendMessageToChannel( message );
+    }
+
+    [Button] public void SendTestMessage() {
+        SendChatMessage( m_testMessage );
     }
 }
